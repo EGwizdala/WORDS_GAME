af5669b98cec4f86471efae32f74653af46c4b49
EGwizdala <emilia.gwizdala@gmail.com>
2020-12-09 13:27:19 +0100

    aside modifications

--- js/main.js
+++ js/main.js
@@ -4,30 +4,31 @@ class Toggle {
             console.log(element)
             element.classList.toggle(this.className)
         })
+    }
 
-       
+    toggleVar() {
+        console.log(this.elements)
+        for(let i = 0; i < this.elements.length; i++ ){
+            this.elements[i].classList.toggle(this.className[i])
+        }
     }
 }
 
 class Aside {
     constructor() { 
-        
-        this.elements = [document.querySelector("aside")];
-        this.className = ["showAside"];
-        this.newToggle = new Toggle();
-
-
-    
-        document.querySelector(".arrow").addEventListener("click", this.newToggle.toggle.bind(this));
-       
-
-
+        this.elements = [document.querySelector("aside"), document.querySelector(".arrow div")];
+        this.className = ["showAside", "arrowRotate"];
+        this.aside = new Toggle();
+        document.querySelector(".arrow").addEventListener("click", this.aside.toggleVar.bind(this));
     }
-
-   
-
-
 }
 
 const aside = new Aside()
 
+
+// class ChangeImage {
+//     this.imgs = 
+// }
+
+var fs = require('fs');
+var files = fs.readdirSync('/assets/photos/');
